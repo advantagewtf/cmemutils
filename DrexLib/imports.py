1a156b2aef87742c5f761d3b288819220322cfb4
Drexxy <patrick@savells.net>
2024-12-04 15:55:32 -0600

    Create imports.py

---
+++ DrexLib/imports.py
@@ -0,0 +1,17 @@
+from ctypes import wintypes
+import time
+import os
+import string
+import random
+import subprocess
+import threading
+import json
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import base64
+import ctypes
+import win32api
+import win32process
+import win32con
+from win32api import OpenProcess
+import requests
